008f303c0438a0ad6ba353ed186434d9445f883f
Charles Lombardo <clombardo169@gmail.com>
2022-04-10 14:00:26 -0400

    Persist fragment state
    When switching between tabs in the BottomNavigationView, each fragment is loaded once and kept in memory to prevent reloading the database repeatedly

--- app/src/main/java/com/example/freebie/MainActivity.java
+++ app/src/main/java/com/example/freebie/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -25,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
     public static MediaPlayer mediaPlayer;
     public static Song currentlyPlayingSong;
 
-    public Fragment homeFragment;
-    public Fragment albumsFragment;
-    public Fragment artistsFragment;
-    public Fragment settingsFragment;
+    public HomeFragment homeFragment;
+    public AlbumsFragment albumsFragment;
+    public ArtistsFragment artistsFragment;
+    public SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,40 +40,33 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         // Create each fragment in advance
-        homeFragment = new HomeFragment();
-        albumsFragment = new AlbumsFragment();
-        artistsFragment = new ArtistsFragment();
-        settingsFragment = new SettingsFragment();
+        if(savedInstanceState == null) {
+            homeFragment = HomeFragment.newInstance();
+            albumsFragment = AlbumsFragment.newInstance();
+            artistsFragment = ArtistsFragment.newInstance();
+            settingsFragment = SettingsFragment.newInstance();
+        }
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-                String fragmentTag = null;
-                // TODO: Find a better way to set tags
                 switch (item.getItemId()) {
                     case R.id.action_home:
-                        fragment = homeFragment;
-                        fragmentTag = "HomeFragment";
+                        displayHomeFragment();
                         break;
                     case R.id.action_albums:
-                        fragment = albumsFragment;
-                        fragmentTag = "AlbumsFragment";
+                        displayAlbumsFragment();
                         break;
                     case R.id.action_artists:
-                        fragment = artistsFragment;
-                        fragmentTag = "ArtistsFragment";
+                        displayArtistsFragment();
                         break;
                     case R.id.action_settings:
-                        fragment = settingsFragment;
-                        fragmentTag = "SettingsFragment";
+                        displaySettingsFragment();
                         break;
                     default:
-                        fragment = homeFragment;
-                        fragmentTag = "HomeFragment";
+                        displayHomeFragment();
                         break;
                 }
-                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment, fragmentTag).commit();
                 return true;
             }
         });
@@ -84,5 +78,65 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCompletion(MediaPlayer mp) { currentlyPlayingSong = null; }
         });
+    }
+
+    protected void displayHomeFragment() {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        if (homeFragment.isAdded()) { // if the fragment is already in container
+            ft.show(homeFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.flContainer, homeFragment, "HomeFragment");
+        }
+        // Hide non-relevant fragments
+        if (albumsFragment.isAdded()) { ft.hide(albumsFragment); }
+        if (artistsFragment.isAdded()) { ft.hide(artistsFragment); }
+        if (settingsFragment.isAdded()) { ft.hide(settingsFragment); }
+        // Commit changes
+        ft.commit();
+    }
+
+    protected void displayAlbumsFragment() {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        if (albumsFragment.isAdded()) { // if the fragment is already in container
+            ft.show(albumsFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.flContainer, albumsFragment, "AlbumsFragment");
+        }
+        // Hide non-relevant fragments
+        if (homeFragment.isAdded()) { ft.hide(homeFragment); }
+        if (artistsFragment.isAdded()) { ft.hide(artistsFragment); }
+        if (settingsFragment.isAdded()) { ft.hide(settingsFragment); }
+        // Commit changes
+        ft.commit();
+    }
+
+    protected void displayArtistsFragment() {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        if (artistsFragment.isAdded()) { // if the fragment is already in container
+            ft.show(artistsFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.flContainer, artistsFragment, "ArtistsFragment");
+        }
+        // Hide non-relevant fragments
+        if (homeFragment.isAdded()) { ft.hide(homeFragment); }
+        if (albumsFragment.isAdded()) { ft.hide(albumsFragment); }
+        if (settingsFragment.isAdded()) { ft.hide(settingsFragment); }
+        // Commit changes
+        ft.commit();
+    }
+
+    protected void displaySettingsFragment() {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        if (settingsFragment.isAdded()) { // if the fragment is already in container
+            ft.show(settingsFragment);
+        } else { // fragment needs to be added to frame container
+            ft.add(R.id.flContainer, settingsFragment, "settingsFragment");
+        }
+        // Hide non-relevant fragments
+        if (homeFragment.isAdded()) { ft.hide(homeFragment); }
+        if (albumsFragment.isAdded()) { ft.hide(albumsFragment); }
+        if (artistsFragment.isAdded()) { ft.hide(artistsFragment); }
+        // Commit changes
+        ft.commit();
     }
 }
