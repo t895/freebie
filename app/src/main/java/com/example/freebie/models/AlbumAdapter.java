ea2b18827567b0ebf3961b04facd9ca3dfa8fe49
Nay Chi Aye Chan <naychiwinny@gmail.com>
2022-04-15 22:02:02 -0400

    Update AlbumAdapter.java

--- app/src/main/java/com/example/freebie/models/AlbumAdapter.java
+++ app/src/main/java/com/example/freebie/models/AlbumAdapter.java
@@ -1,8 +1,14 @@
 package com.example.freebie.models;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.freebie.R;
 
 import java.util.ArrayList;
 
@@ -10,13 +16,22 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyViewHolder
 
     private Context context;
     private ArrayList<Song> albumFiles;
+    View view;
 
     public AlbumAdapter(Context mContext, ArrayList<Song> albumFiles){
-        this.context = mContext;
+        this.context = context;
         this.albumFiles = albumFiles;
 
-    }
-    public class MyViewHolder {
 
+    }
+    public class MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent,int viewType){
+        view = LayoutInflater.from(context).inflate(R.layout.albumItem, parent, false);
+        return new MyViewHolder(view);
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return albumFiles.size();
     }
 }
